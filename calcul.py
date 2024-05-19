e19e995c32ddcdb346c66653601dcaef4d45749a
unknown <cosminelsen1991.aliencosminel@gmail.com>
2024-05-19 16:23:18 +0300

    First commit

---
+++ calcul.py
@@ -0,0 +1,55 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 09:44:33) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+5
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+True
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+False
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+1.52
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+(1+2j)
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+None
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+test
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+element
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+element1
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+element1
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+['element1', 'element2', 'element3']
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+Traceback (most recent call last):
+  File "C:/Users/CORSAIR/Documents/modul 2lectia2.py", line 3, in <module>
+    print (list1)
+NameError: name 'list1' is not defined
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+('element1', 'element2')
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+{1, 2, 3}
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+{'key1': 'element1', 'key2': 'element2'}
+>>> 
+============= RESTART: C:/Users/CORSAIR/Documents/modul 2lectia2.py ============
+{'key1': 'element1', 'key2': 'element2'}
+element1
+>>> 
